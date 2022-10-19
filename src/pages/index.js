684bb8a52bf2ad3d23fe869509ef50f479a04011
Chrisloic <chrisloic.dev@gmail.com>
2022-10-19 10:33:06 -0400

    19 oct 2022 few modif

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 //import { Link } from "gatsby"
 import Layout from "../components/layout"
+
+import image from "../images/loulou.jpeg"
 // import { StaticImage } from 'gatsby-plugin-image'
 //import headshot from "../components/layout.module.css'"
 
@@ -190,7 +192,10 @@ return (
     <p>I have a wide range of literary tastes.</p>
 
     <p>And I make playlists that I may share with you.</p>
-        
+
+   
+     
+
   </Layout>
 )
 
@@ -198,7 +203,8 @@ return (
   ///  <h1>Welcome to my Gatsby site!</h1>
   ///  <Link to="/about">About</Link>
   ///  <p>I'm making this by following the Gatsby Tutorial.</p>
- /// </main>
+ /// </main> 
+ // 
 
 }
 
